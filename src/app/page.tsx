736276c0d094e23643c39ae7d84bf721711ab225
ohene gyan <ohenegyan159@gmail.com>
2025-09-05 00:37:25 +0000

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -605,6 +605,28 @@ const Page = () => {
   }, [showPostModal]);
 
 
+  // Prevent body scrolling on mobile when experience is visible
+  useEffect(() => {
+    if (experienceVisible) {
+      document.body.style.overflow = 'hidden';
+      document.body.style.height = '100vh';
+      document.documentElement.style.overflow = 'hidden';
+      document.documentElement.style.height = '100vh';
+    } else {
+      document.body.style.overflow = '';
+      document.body.style.height = '';
+      document.documentElement.style.overflow = '';
+      document.documentElement.style.height = '';
+    }
+    
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.height = '';
+      document.documentElement.style.overflow = '';
+      document.documentElement.style.height = '';
+    };
+  }, [experienceVisible]);
+
   useEffect(() => {
     // Don't start the loader timer if coming from Windows
     if (fromWindows) return;
@@ -816,7 +838,7 @@ const Page = () => {
 
 
   return (
-    <div ref={pageRef} className="relative ">
+    <div ref={pageRef} className="relative" style={{ height: '100vh', overflow: 'hidden' }}>
       {/* Experience page hidden behind */}
       {experienceVisible && ( 
         <div
