424087d9f5426ce2a1fe1c24c92278d3c59dda34
ohene gyan <ohenegyan159@gmail.com>
2025-09-26 16:48:31 +0000

    new

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -226,20 +226,10 @@ function PaymentSuccessContent() {
           </div>
 
           {/* Important Notice */}
-          <div className="bg-red-500/20 border border-red-500/30 rounded-lg p-4 mb-4">
-            <div className="flex items-start space-x-3">
-              <div className="w-5 h-5 bg-red-500 rounded-full flex items-center justify-center flex-shrink-0 mt-0.5">
-                <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-              </div>
-              <div>
-                <p className={`${roboto.className} text-red-100 text-sm font-medium mb-1`}>Important Notice</p>
-                <p className={`${roboto.className} text-red-100 text-xs leading-relaxed`}>
-                  Please do not share this link with others as it violates our community rules. This link is exclusively for your personal use.
-                </p>
-              </div>
-            </div>
+          <div className="mt-8">
+            <p className={`${roboto.className} text-white text-sm font-bold text-center leading-relaxed`}>
+              Please do not share this link with others as it violates our community rules. This link is exclusively for your personal use.
+            </p>
           </div>
         </div>
       </div>
